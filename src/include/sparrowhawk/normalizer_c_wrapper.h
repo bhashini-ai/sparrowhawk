bff02030b27acb569ce58a692c475e947447b91d
Shiva Kumar H R <shivahr@gmail.com>
2025-01-13 20:23:26 +0530

    Simple C-wrapper for calling from Java FFM API

---
+++ src/include/sparrowhawk/normalizer_c_wrapper.h
@@ -0,0 +1,15 @@
+#ifndef SPARROWHAWK_NORMALIZER_C_WRAPPER_H_
+#define SPARROWHAWK_NORMALIZER_C_WRAPPER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int Normalizer_Setup(const char *configuration_proto, const char *pathname_prefix);
+int Normalizer_Normalize(const char *input, char **output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // SPARROWHAWK_NORMALIZER_C_WRAPPER_H_
